af1c0f3e5a0cd6046d6ee2680a36022e0a976800
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-04-25 10:18:16 +0900

    initial commit

---
+++ src/main/java/com/example/oneinkedoneproject/OneinkedOneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneinkedoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneinkedOneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneinkedOneProjectApplication.class, args);
+    }
+
+}
