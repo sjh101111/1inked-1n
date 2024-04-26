6c2682396ffcc807b7f7c9ce9aedd9eac2129beb
lth01 <dlxogml11235@naver.com>
2024-04-26 14:42:55 +0900

    Feat: 비회원 메인 화면 개발
    1. 레이아웃 퍼블리싱
    2. mock 메서드 개발
    3. 라우팅 설정
    
    Resolve: #9

--- src/front/src/App.jsx
+++ src/front/src/App.jsx
@@ -1,13 +1,24 @@
 import { GlobalContext, ContextProvider } from ".";
 import { CookiesProvider } from 'react-cookie';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from "./routes/Main";
+import Login from "./routes/Login";
+import Signup from "./routes/Signup";
 
 
 const router = createBrowserRouter([
       {
         path: "/",
-        element: <div></div> 
+        element: <Main></Main> 
       },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>
+      }
 ]);
 
 export default function App(){
---
+++ src/front/src/routes/Main.jsx
@@ -0,0 +1,60 @@
+import { useContext, useEffect } from "react";
+import { GlobalContext } from "..";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button"
+
+
+const Main = () =>{
+    //라우팅 네비게이터
+    const nevigate = useNavigate();
+
+    //글로벌 컨텍스트
+    const { isLogin, setLogin } = useContext(GlobalContext);
+
+    //초기 설정
+    useEffect(() =>{
+    },[isLogin]);
+
+    const doLogin = () =>{
+            setLogin(true);
+        }
+
+    const goSignup = () =>{
+            nevigate("/signup");
+        }
+
+    return (
+        isLogin ?
+        <div>로그인 된거</div> :
+        (<main className="flex h-screen">
+            <section className=" flex-grow-[6] bg-[#6866EB] px-16 py-24 text-white">
+                <h1 className="text-6xl font-bold">About Us</h1>
+                <div className="mt-48 text-4xl font-bold grid gap-4">
+                    <h2>1</h2>
+                    <h2>2</h2>
+                    <h2>3</h2>
+                    <h2>4</h2>
+                </div>
+            </section>
+            <section className="flex-grow-[4] login flex flex-col items-center justify-center">
+                <div className="w-full text-4xl font-bold text-white">
+                    <h2 className="text-center text-black">Get Started</h2>
+                    <div className="flex justify-center gap-4 mt-4">
+                        <Button className="bg-[#6866EB] w-48 hover:bg-violet-600">
+                            <div onClick={doLogin}>
+                                Login
+                            </div>
+                        </Button>
+                        <Button className="bg-[#6866EB] w-48 hover:bg-violet-600">
+                            <div onClick={goSignup}>
+                                Signup
+                            </div>
+                        </Button>
+                    </div>
+                </div>
+            </section>
+            </main>)
+    );
+};
+
+export default Main;
