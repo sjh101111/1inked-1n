38941d399441feac29e6c5295ad982299eaae1a8
cmleem <coals722@naver.com>
2024-05-01 22:18:51 +0900

    comment 엔티티 서비스 계층에서 단위테스트 완료

---
+++ src/main/java/com/example/oneinkedoneproject/dto/UpdateCommentRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.oneinkedoneproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateCommentRequestDto {
+    private String comments;
+}
