677cede792ae509cee97637db298023b1a1d2a6d
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-05-03 14:28:46 +0900

    Fix: JwtAuthenticationFilter 액세스 토큰의 상황에 따라 메세지 추가

--- src/main/java/com/example/oneinkedoneproject/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/oneinkedoneproject/filter/JwtAuthenticationFilter.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.stereotype.Component;
 
@@ -34,13 +33,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
             @NonNull final HttpServletRequest request,
             @NonNull final HttpServletResponse response,
             @NonNull final FilterChain filterChain)
-    throws ServletException, IOException {
-        //final String token = getParseJwt(request.getHeader("Authorization"));
+            throws ServletException, IOException {
+
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
 
-        // JWT 토큰이 존재하는지 확인. 존재 안하면 컼ㅅ.
+        // JWT 토큰이 존재하는지 확인. 존재 안하면 컷
         if(authHeader == null ||authHeader.startsWith(("Bearer "))){
             filterChain.doFilter(request, response);
             return;
@@ -48,36 +47,39 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
         //유저의 이메일이 존재하면서 유저가 인증받지 않았다면
-        if(userEmail !=null && SecurityContextHolder.getContext().getAuthentication()==null){
+        if(userEmail !=null && SecurityContextHolder.getContext().getAuthentication()==null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
 
-            //토큰이 valid하다면
-            if(jwtService.isTokenValid(jwt, userDetails)){
+            try {
+                //토큰이 valid하다면
+                if (jwtService.isTokenValid(jwt, userDetails)) {
 
-                //authentication 토큰 생성
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                //요청에 대한 세부사항을 토큰에 업데이트
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                //토큰을 security contextholder에 업데이트
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                    //authentication 토큰 생성
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    //요청에 대한 세부사항을 토큰에 업데이트
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    //토큰을 security contextholder에 업데이트
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                } else {//액세스 토큰이 valid하지 않다면 invalid 메세지 추가
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Acess token is invalid or expired");
+                    return;
+                }
+            }
+            catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write("Could not cain Authentication");
+                return;
             }
         }
         filterChain.doFilter(request, response);
-
-    }
-    private String getParseJwt(final String headerAuth) {
-
-        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer")) {
-            return headerAuth.substring(7);
-        }
-
-        return null;
     }
 }
