03d4c12f2ee2f2326f7b68ae725fd936fa463ec1
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-05-08 10:59:46 +0900

    Feat: ResponseEntity 리턴하도록 설정

--- src/main/java/com/example/oneinkedoneproject/controller/ExControllerAdvice.java
+++ src/main/java/com/example/oneinkedoneproject/controller/ExControllerAdvice.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,29 +18,29 @@ public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({IllegalArgumentException.class, BadRequestException.class})
-    public ErrorResult badRequestExHandle(Exception e) {
+    public ResponseEntity<ErrorResult> badRequestExHandle(Exception e) {
         log.error("[exceptionHandle] badRequestExHandle", e);
-        return new ErrorResult("잘못된 입력", e.getMessage());
+        return new ResponseEntity<ErrorResult>( new ErrorResult("잘못된 입력", e.getMessage()),HttpStatus.BAD_REQUEST) ;
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
-    public ErrorResult runtimeExHandle(RuntimeException e) {
+    public ResponseEntity<ErrorResult> runtimeExHandle(RuntimeException e) {
         log.error("[exceptionHandle] RuntimeExHandle", e);
-        return new ErrorResult("잘못된 사용자", e.getMessage());
+        return new ResponseEntity<ErrorResult>( new ErrorResult("잘못된 사용자", e.getMessage()),HttpStatus.BAD_REQUEST) ;
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
-    public ErrorResult NotFoundExHandle(EntityNotFoundException e) {
+    public ResponseEntity<ErrorResult> NotFoundExHandle(EntityNotFoundException e) {
         log.error("[exceptionHandle] EntityNotFoundExHandle", e);
-        return new ErrorResult("존재하지 않는 데이터", e.getMessage());
+        return new ResponseEntity<ErrorResult>(  new ErrorResult("존재하지 않는 데이터", e.getMessage()),HttpStatus.BAD_REQUEST) ;
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
-    public ErrorResult AccessDeniedExHandle(AccessDeniedException e) {
+    public ResponseEntity<ErrorResult> AccessDeniedExHandle(AccessDeniedException e) {
         log.error("[exceptionHandle] AccessDeniedExHandle", e);
-        return new ErrorResult("권한 오류", "접근 권한이 없습니다.");
+        return new ResponseEntity<ErrorResult>( new ErrorResult("권한 오류", "접근 권한이 없습니다."),HttpStatus.BAD_REQUEST) ;
     }
 }
