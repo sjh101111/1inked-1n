50851b8b14cd9d9188fc8fb2fb42dd0d07928670
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-05-08 16:59:15 +0900

    Fix: JwTRefreshTokenFilter 에서 ErrorResult로 에러 메세지를 전달받도록 테스트 수정

--- src/main/java/com/example/oneinkedoneproject/filter/JwtRefreshTokenFilter.java
+++ src/main/java/com/example/oneinkedoneproject/filter/JwtRefreshTokenFilter.java
@@ -1,13 +1,16 @@
 package com.example.oneinkedoneproject.filter;
 
+import com.example.oneinkedoneproject.dto.auth.ErrorResult;
 import com.example.oneinkedoneproject.dto.auth.TokenInfo;
 import com.example.oneinkedoneproject.service.auth.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,51 +23,69 @@ import java.io.IOException;
 public class JwtRefreshTokenFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private HttpServletResponse response;
+    private final ObjectMapper objectMapper= new ObjectMapper();
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
+            @NonNull HttpServletResponse responseParam,
             @NonNull FilterChain filterChain)
             throws IOException, ServletException {
 
+        this.response = responseParam;
 
         //경로에 /refresh가 없다면 다음 필터체인으로 이동
         final String requestURI = request.getRequestURI();
         if(!"/refresh".equals(requestURI)){
-            filterChain.doFilter(request, response);
+            filterChain.doFilter(request, responseParam);
+            return;
         }
 
         final String authHeader = request.getHeader("Refresh-Token");
 
-        if (authHeader == null || !authHeader.startsWith(("Bearer "))) {//일단 토큰은 존재
-            unauthorizedResponse(response, "Unauthorized: No valid Bearer token provided");
+        if(authHeader == null){ // 액세스 토큰이  아예 존재하지 않거나, Bearer로 시작하지 않고있다면
+            createUnauthorizedResponse(HttpServletResponse.SC_UNAUTHORIZED,
+                    "JWT refresh Token Error", "No token provided");
             return;
         }
+
+        if(!authHeader.startsWith(("Bearer "))){ // 액세스 토큰이  아예 존재하지 않거나, Bearer로 시작하지 않고있다면
+            createUnauthorizedResponse(HttpServletResponse.SC_UNAUTHORIZED,
+                    "JWT refresh Token Error", "No valid Bearer token provided");
+            return;
+
+        }
+
         String refreshToken = authHeader.substring(7);
         String userEmail = jwtService.extractUsername(refreshToken);
         if (userEmail == null) {
-            unauthorizedResponse(response, "Unauthorized: token does not contain a valid email");
+            createUnauthorizedResponse(HttpServletResponse.SC_UNAUTHORIZED,
+                    "JWT refresh Token Error", "token does not contain a valid email");
             return;
         }
         UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
         try {
             if (!jwtService.isTokenValid(refreshToken, userDetails)) {//토큰이 valid하지 않을 때
-                unauthorizedResponse(response, "Refresh token is invalid or expired");
+                createUnauthorizedResponse(HttpServletResponse.SC_UNAUTHORIZED,
+                        "JWT refresh Token Error", "Refresh token is invalid or expired");
                 return;
             }
         }catch(Exception e){
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            response.getWriter().write("Could not gain Authentication");
+            createUnauthorizedResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "JWT refresh Token Error", "Could not gain Authentication");
             return;
         }
 
         TokenInfo newTokenInfo = jwtService.generateTokenInfo(userDetails);//새로운 토큰을 생성 후 리스폰스 헤더에 설정
+        response.setStatus(HttpServletResponse.SC_OK);
         response.setHeader("Authorization", "Bearer " + newTokenInfo.getAccessToken());
         response.setHeader("Refresh-Token", "Bearer " + newTokenInfo.getRefreshToken());
     }
-    private void unauthorizedResponse(HttpServletResponse response, String message) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);//401 상태코드 리턴
-        response.getWriter().write(message);
+
+    private void createUnauthorizedResponse(int httpResponse, String error, String message) throws IOException {
+        response.setStatus(httpResponse);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(new ErrorResult(error,  message)));
         response.getWriter().flush();
     }
 }
