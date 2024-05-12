5e0393ef2992ae4ac1620051d4862babb52490a2
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-05-13 06:16:31 +0900

    Fix: NaverNewsService에서 throw한 에러를 처리하도록 설정

--- src/main/java/com/example/oneinkedoneproject/controller/externalApi/NaverNewsController.java
+++ src/main/java/com/example/oneinkedoneproject/controller/externalApi/NaverNewsController.java
@@ -7,12 +7,13 @@ import com.example.oneinkedoneproject.service.externalApi.NaverNewsApiService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @RestController
@@ -21,25 +22,19 @@ import java.util.List;
 public class NaverNewsController {
     private final NaverNewsApiService naverNewsApiService;
 
-    @GetMapping("/naver-news/blah")
-    public ResponseEntity<String> naverAPITest(){
-        return ResponseEntity.status(HttpServletResponse.SC_OK).body("okok");
-    }
     @GetMapping("/naver-news")
-    public ResponseEntity<List<NaverNewsItemDto>> getNewsItems(@ModelAttribute NaverNewsApiRequest request) throws UnsupportedEncodingException {
+    public ResponseEntity<List<NaverNewsItemDto>> getNewsItems(@ModelAttribute NaverNewsApiRequest request) throws BadRequestException {
         String requestUrl = naverNewsApiService.apiRequestUrlGenerator(request);
+        try {
         NaverNewsApiResponse response = naverNewsApiService.fetchNewsFromNaverAPI(requestUrl);
         ResponseEntity<List<NaverNewsItemDto>> responseToFrontend;
-        if (response != null) {
+
             responseToFrontend = ResponseEntity
                     .status(HttpServletResponse.SC_OK)
                     .body(response.getItems());
             return responseToFrontend;
-        } else {
-            responseToFrontend = ResponseEntity
-                    .status(HttpServletResponse.SC_BAD_REQUEST)
-                    .body(null);
-            return responseToFrontend;
+        }catch(HttpClientErrorException e) {
+            throw new BadRequestException("부적절한 인자가 전달되었습니다.");
         }
     }
 
