8e84ec76a96d7bc595b8119ceb18351eb8787f4e
서창덕 <44630705+Garodden@users.noreply.github.com>
2024-05-13 03:23:08 +0900

    Feat: NaverNewsApiService 클라이언트의 요청에 따라서 네이버 API에 쿼리 파라미터가 포함된 URL로 요청 후, 받은 정보를 리턴해주는 로직 구현

---
+++ src/main/java/com/example/oneinkedoneproject/service/externalApi/NaverNewsApiService.java
@@ -0,0 +1,63 @@
+package com.example.oneinkedoneproject.service.externalApi;
+
+import com.example.oneinkedoneproject.dto.externalApi.NaverNewsApiRequest;
+import com.example.oneinkedoneproject.dto.externalApi.NaverNewsApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+@RequiredArgsConstructor
+@Component
+public class NaverNewsApiService {
+    private final RestTemplate restTemplate;
+    @Value("${naver-client}")
+    String naverClient;
+
+    @Value("${naver-key}")
+    String naverSecret;
+
+    public String apiRequestUrlGenerator(NaverNewsApiRequest request) throws UnsupportedEncodingException {
+        byte[] urlQuery = request.getQuery().getBytes("UTF-8");//검색어를 UTF-8로 인코딩
+
+        int urlDisplay= 20; // 한 페이지 기사 20개, 총 100개의 기사, 5페이지
+        int urlStart = 20*(request.getPage()-1);//
+        String urlSort = request.getSort();
+
+        String requestUrl = "https://openapi.naver.com/v1/search/news.json";//요청 URL
+        requestUrl = requestUrl
+                +"?query="+ new String(urlQuery)
+                +"&display="+ Integer.toString(urlDisplay)
+                +"&start="+ Integer.toString(urlStart)
+                +"&sort="+urlSort;
+        return requestUrl;
+    }
+
+    public NaverNewsApiResponse fetchNewsFromNaverAPI(String apiUrl){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", naverClient);
+        headers.set("X-Naver-Client-Secret", naverSecret);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<NaverNewsApiResponse> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.GET,
+                entity,
+                NaverNewsApiResponse.class
+        );
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
+}
