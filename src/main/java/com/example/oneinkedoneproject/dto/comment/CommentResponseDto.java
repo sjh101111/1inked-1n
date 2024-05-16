f54b1b486dfa493c883013985ef5102e2f9f15fa
cmleem <coals722@naver.com>
2024-05-16 09:39:55 +0900

    comment dto 리팩토링

--- src/main/java/com/example/oneinkedoneproject/dto/comment/CommentResponseDto.java
+++ src/main/java/com/example/oneinkedoneproject/dto/comment/CommentResponseDto.java
@@ -1,8 +1,7 @@
-package com.example.oneinkedoneproject.dto;
+package com.example.oneinkedoneproject.dto.comment;
 
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import com.example.oneinkedoneproject.domain.Comment;
 
